8c0a4f8475defd374e06f1fe7ac9758a2ee58687
Jhonathas <jhonathassouza8@gmail.com>
2023-03-08 21:55:39 -0300

    criando validacao de autenticacao e resgatando usuario autenticado

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const {register} = require("../controllers/UserController.js");
+const {register, login, getCurrentUser} = require("../controllers/UserController.js");
+const authGuard = require("../middlewares/authGuard");
+
+//middlewares
+const validate = require("../middlewares/handleValidation")
+const {useCreateValidation, loginValidation} = require("../middlewares/userValidation")
 
 // Routes
-router.post("/register", register);
+router.post("/register", useCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser);
+
 
 module.exports = router;
